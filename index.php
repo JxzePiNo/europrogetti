8b9978212c9493e6bd21e5c6e76ae55909ecc3d1
JxzePiNo <86833020+JxzePiNo@users.noreply.github.com>
2022-01-15 14:54:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php/*
+<?php /*
 // server IMAP per la connessione
 $server = '{imapmail.libero.it:993/ssl/novalidate-cert}Spam';
 // account Gmail
@@ -45,7 +45,7 @@ for($i = 1; $i <= $conteggio; $i++) {
 	/*
 	$corpoesploso = explode("quoted-printable", $corpo_dec);
 	$mess = $corpoesploso[2];
-    $text = explode('>', $mess);*/
+    $text = explode('>', $mess);
 	$stringa = explode("//", $corpo_dec);
 	$contenuto = explode(":", $stringa[1]);
 	
@@ -88,7 +88,7 @@ for($i = 1; $i <= $conteggio; $i++) {
 /*
 	PROBLEMI DA RISOLVERE:
 	-HOSTING CON IMAP	
-	*/
+	
 	imap_delete($connessione, $conteggio);
 	
 imap_close($connessione);
@@ -230,7 +230,7 @@ $conn->close(); */
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical"><circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/></svg>
         </button></div> 
 		</div> 
-          <?php 
+          <?php /*
 			$servername = "localhost";
 			$username = "root";
 			$password = "";
@@ -284,7 +284,7 @@ $conn->close(); */
 			';
 	    }
 			}
-	  ?>
+	  */?>
 	  </div>
   </div>
 </div>
